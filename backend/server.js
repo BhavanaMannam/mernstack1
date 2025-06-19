52f65773d65c590c503a5e15fb6f27a1abd823b0
BhavanaMannam <bhavanamannam313@gmail.com>
2025-06-19 14:24:52 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://mannambhavana313:bhavanamannam@cluster0.xys1lpi.mongodb.net/', {
+mongoose.connect('mongodb+srv://mannambhavana313:bhavanamannam@cluster0.xys1lpi.mongodb.net/mytodosdb?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Root route to avoid "Cannot GET /"
+app.get('/', (req, res) => {
+  res.send('Backend server is running');
 });
 
 app.use('/api/todos', todoRoutes);
